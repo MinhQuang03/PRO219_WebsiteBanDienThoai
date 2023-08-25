f46b8f43287711c4041cef2f15ad7f1b4d7e162e
thanh05012003 <92389586+thanh05012003@users.noreply.github.com>
2023-08-25 21:36:52 +0700

    n

--- PRO219_WebsiteBanDienThoai/PRO219_WebsiteBanDienThoai/Models/ChargingportType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PRO219_WebsiteBanDienThoai.Models
-{
-    public class ChargingportType
-    {
-        public Guid Id { get; set; }
-
-        public string? Name { get; set; }
-    }
-}
--- PRO219_WebsiteBanDienThoai/PRO219_WebsiteBanDienThoai/Models/Color.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PRO219_WebsiteBanDienThoai.Models
-{
-    public class Color
-    {
-        public Guid Id { get; set; }
-
-        public string? Name { get; set; }
-    }
-}
