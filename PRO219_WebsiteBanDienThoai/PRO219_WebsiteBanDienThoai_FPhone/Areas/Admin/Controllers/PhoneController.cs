0ff74065bef852c6e0c4e7ec4bbf16567dd8aa71
2 am Quang <86760575+MinhQuang03@users.noreply.github.com>
2023-08-28 21:49:18 +0700

    update-123

--- PRO219_WebsiteBanDienThoai/PRO219_WebsiteBanDienThoai_FPhone/Areas/Admin/Controllers/PhoneController.cs
+++ PRO219_WebsiteBanDienThoai/PRO219_WebsiteBanDienThoai_FPhone/Areas/Admin/Controllers/PhoneController.cs
@@ -20,6 +20,21 @@ namespace PRO219_WebsiteBanDienThoai_FPhone.Areas.Admin.Controllers
         {
             var datajson = await _httpClient.GetStringAsync("api/Phone/get");
             var obj = JsonConvert.DeserializeObject<List<Phone>>(datajson);
+
+            var productionCompanyNames = new Dictionary<Guid, string>();
+
+            foreach (var phone in obj)
+            {
+                if (!productionCompanyNames.ContainsKey(phone.IdProductionCompany))
+                {
+                    var productionCompanyData = await _httpClient.GetStringAsync($"api/ProductionCompany/getById/{phone.IdProductionCompany}");
+                    var productionCompany = JsonConvert.DeserializeObject<ProductionCompany>(productionCompanyData);
+                    productionCompanyNames.Add(phone.IdProductionCompany, productionCompany.Name);
+                }
+            }
+
+            ViewBag.ProductionCompanyNames = productionCompanyNames;
+
             return View(obj);
         }
         public async Task<IActionResult> Create()
@@ -48,6 +63,43 @@ namespace PRO219_WebsiteBanDienThoai_FPhone.Areas.Admin.Controllers
             var jsonData = JsonConvert.SerializeObject(obj);
             HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
             var response = await _httpClient.PostAsync("api/Phone/add", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(jsonData);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var data = await _httpClient.GetStringAsync("api/ProductionCompany/get");
+            List<ProductionCompany> a = JsonConvert.DeserializeObject<List<ProductionCompany>>(data);
+            ViewBag.IdProductionCompany = new SelectList(a, "Id", "Name");
+
+            var datajson = await _httpClient.GetStringAsync($"api/Phone/getById/{id}");
+            var obj = JsonConvert.DeserializeObject<Phone>(datajson);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Phone obj, IFormFile file)
+        {
+            if (file != null && file.Length > 0) // khong null va khong trong 
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", fileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                obj.Image = "~/img/" + fileName;
+            }
+            var jsonData = JsonConvert.SerializeObject(obj);
+            HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync("api/Phone/update", content);
             if (response.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
