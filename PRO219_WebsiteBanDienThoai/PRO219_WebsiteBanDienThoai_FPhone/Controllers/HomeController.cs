ff488c821ef2c1d1bd762b49e7dc00e1d9b5ac86
NXHoangVu <vunxhph19692@fpt.edu.vn>
2023-08-29 11:56:27 +0700

    3

--- PRO219_WebsiteBanDienThoai/PRO219_WebsiteBanDienThoai_FPhone/Controllers/HomeController.cs
+++ PRO219_WebsiteBanDienThoai/PRO219_WebsiteBanDienThoai_FPhone/Controllers/HomeController.cs
@@ -22,10 +22,6 @@ namespace PRO219_WebsiteBanDienThoai_FPhone.Controllers
         {
             return View();
         }
-        public IActionResult BlogManagement()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
